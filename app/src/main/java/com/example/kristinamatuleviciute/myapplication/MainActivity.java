9b8f5b9d461222539f612876ff571188112c1f29
Kristina Matuleviciute <kristinamatuleviciute@Kristinas-MacBook-Pro.local>
2018-05-12 20:36:03 +0100

    toolbar

--- app/src/main/java/com/example/kristinamatuleviciute/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kristinamatuleviciute/myapplication/MainActivity.java
@@ -4,8 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -60,6 +64,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
         listViewAuthors.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -84,6 +92,33 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this, "Item1 selected", Toast.LENGTH_SHORT).show();
+                        return true;
+            case R.id.item2:
+                Toast.makeText(this, "Item2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "Item3 selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+             default: return super.onOptionsItemSelected(item);
+
+
+        }
     }
 
     @Override
